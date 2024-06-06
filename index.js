6b36e32142b5bf2dab93603e90b0bb5bd4cced3c
Thomas Kruithof <tkruithof95@gmail.com>
2024-06-06 17:27:27 +0200

    Solved lab

--- index.js
+++ index.js
@@ -6,8 +6,8 @@
 const string1 = "My favorite dessert is jello";
 
 // Your code here...
-
-
+const index = string1.indexOf("j")
+console.log(`1. j index: ${index}`)
 
 
 /*******************************************
@@ -18,7 +18,31 @@ const string1 = "My favorite dessert is jello";
 const string2 = "ABCDEFGHJKLO";
 
 // Your code here...
+let newString = ""
 
+for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < string2.length; j++) {
+        if (newString.length === 0) {
+            if (string2[j] === "C") {
+                newString += string2[j];
+                break;
+            }
+        }
+        else if (newString.length < 3) {
+            if (string2[j] === "O") {
+                newString += string2[j];
+                break;
+            }
+        }
+        else {
+            if (string2[j] === "L") {
+                newString += string2[j];
+                break;
+            }
+        }
+    }
+}
+console.log(`2. new string: ${newString}`);
 
 
 
@@ -31,7 +55,7 @@ const string3 = "Na";
 
 // Your code here...
 
-
+console.log(`3. new string: ${string3.repeat(4)} Batman!`)
 
 
 /*******************************************
@@ -42,7 +66,10 @@ const string3 = "Na";
 const fruit = "banana apple mango orange lemon kiwi watermelon grapes pear pineapple";
 
 // Your code here...
+let favoriteFruit = "watermelon";
 
+
+console.log(`4. favorite fruit: ${fruit.slice(fruit.indexOf("watermelon"), (fruit.indexOf("watermelon") + favoriteFruit.length))}`)
 
 
 /***************************************************
@@ -58,11 +85,21 @@ const funnyHeadline2 = "Students Cook & Serve Grandparents";
 
 // Check the first headline
 // Your code here ...
+const wordToCheck = "oxygen";
 
+function checkString(string, word) {
+    if (string.includes(word)) {
+        return `The string includes the word '${word}'.`;
+    }
+    else {
+        return `The string does not include the word '${word}'.`;
+    }
+}
 
 // Check the second headline
 // Your code here ...
-
+console.log(`5a first string: ${checkString(funnyHeadline1, wordToCheck)}`);
+console.log(`5b second string: ${checkString(funnyHeadline2, wordToCheck)}`)
 
 
 /*******************************************
@@ -75,7 +112,9 @@ const string4 = "zEAWrTC9EgtxmK9w1";
 
 // a) Print the string length
 // Your code here ...
+console.log(`6a. length of string: ${string4.length}`);
 
 
 // b) Print the last character in the string
 // Your code here ...
+console.log(`6b. last character: ${string4[string4.length - 1]}`)
